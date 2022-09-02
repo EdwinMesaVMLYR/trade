e85970d071c9e6a1f2d67e8e320b54702e66138a
programarf <programarf@gmail.com>
2022-09-02 10:40:29 -0500

    feature: url csv

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,17 +3,46 @@ import { usePapaParse } from 'react-papaparse'
 
 function Login () {
   const { readRemoteFile } = usePapaParse()
-
+  const config = {
+    delimiter: '',
+    newline: '',
+    quoteChar: '',
+    escapeChar: '',
+    header: true,
+    transformHeader: undefined,
+    dynamicTyping: false,
+    preview: 0,
+    encoding: '',
+    worker: false,
+    comments: false,
+    step: undefined,
+    complete: undefined,
+    error: undefined,
+    download: false,
+    downloadRequestHeaders: undefined,
+    downloadRequestBody: undefined,
+    skipEmptyLines: false,
+    chunk: undefined,
+    chunkSize: undefined,
+    fastMode: undefined,
+    beforeFirstChunk: undefined,
+    withCredentials: undefined,
+    transform: undefined
+  }
+  console.log(config)
   const handleReadRemoteFile = () => {
-    readRemoteFile('https://react-papaparse.js.org/static/csv/normal.csv', {
+    readRemoteFile('https://dev-site-andrespereira.pantheonsite.io/sites/default/files/2022-09/3.csv', {
+      header: true,
       complete: (results) => {
         console.log('---------------------------')
-        console.log('Results ssgit remote add origin git@github.com:EdwinMesaVMLYR/trade.git:', results)
+        console.log('Results:', results.data)
         console.log('---------------------------')
       }
     })
   }
-
-  return <button onClick={() => handleReadRemoteFile()}>readRemoteFile</button>
+  console.log(handleReadRemoteFile())
+  return (
+    <div>reading</div>
+  )
 }
 export default Login
