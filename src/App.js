460866012c486ca15cf69bf984bed359f8a53ac9
programarf <programarf@gmail.com>
2022-09-06 18:34:32 -0500

    route

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {
   Route
 } from 'react-router-dom'
 import Layout from './Layout/Layout'
-import Login from './pages/Login/Login'
 import Trade from './pages/Trade/Trade'
 
 function App () {
@@ -14,8 +13,7 @@ function App () {
       <Layout>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/trade" element={<Trade />} />
+            <Route path="/" element={<Trade />} />
           </Routes>
         </BrowserRouter>
       </Layout>
