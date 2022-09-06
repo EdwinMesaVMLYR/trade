61599854aff00c29c68909623f459bede979a295
programarf <programarf@gmail.com>
2022-09-06 18:41:25 -0500

    xlsx

--- src/Hooks/userReadBannerXlsx.js
+++ src/Hooks/userReadBannerXlsx.js
@@ -8,7 +8,7 @@ export function userReadBannerXlsx (marca) {
   /* Fetch and update the state once */
   useEffect(() => {
     (async () => {
-      const f = await (await fetch('https://dev-site-andrespereira.pantheonsite.io/sites/default/files/2022-09/banner-marcas_0.xlsx')).arrayBuffer()
+      const f = await (await fetch('https://dev-site-andrespereira.pantheonsite.io/sites/default/files/2022-09/banner-marcas_1.xlsx')).arrayBuffer()
       const wb = read(f) // parse the array buffer
       const ws = wb.Sheets[wb.SheetNames[0]] // get the first worksheet
       const data = utils.sheet_to_json(ws) // generate objects
