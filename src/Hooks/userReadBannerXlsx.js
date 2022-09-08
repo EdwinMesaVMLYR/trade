4459282d028d02520e166a9c2e212636fa2ba621
programarf <programarf@gmail.com>
2022-09-08 17:11:09 -0500

    fix: cors

--- src/Hooks/userReadBannerXlsx.js
+++ src/Hooks/userReadBannerXlsx.js
@@ -4,14 +4,22 @@ import { read, utils } from 'xlsx'
 export function userReadBannerXlsx (marca) {
   /* the component state is an array of presidents */
   const [pres, setPres] = useState([])
-
+  const url = 'https://dev-ab-co-budweiser.pantheonsite.io/sites/default/files/2022-09/banner-marcas.xlsx'
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    redirect: 'follow'
+  }
   /* Fetch and update the state once */
   useEffect(() => {
     (async () => {
-      const f = await (await fetch('https://dev-ab-co-budweiser.pantheonsite.io/sites/default/files/2022-09/banner-marcas.xlsx')).arrayBuffer()
+      const f = await (await fetch(url, requestOptions)).arrayBuffer()
       const wb = read(f) // parse the array buffer
       const ws = wb.Sheets[wb.SheetNames[0]] // get the first worksheet
       const data = utils.sheet_to_json(ws) // generate objects
+      console.log('debud: ', data)
       setPres(data) // update state
     })()
   }, [])
