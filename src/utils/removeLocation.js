621be0dceb95475b9384ab8e6d560f244edbfdf0
programarf <programarf@gmail.com>
2022-09-13 15:52:27 -0500

    fix: new catalogue update

--- src/utils/removeLocation.js
+++ src/utils/removeLocation.js
@@ -3,7 +3,5 @@ export function removeLocation (text, acronym) {
   const LowerCase = text?.toLowerCase() || ''
   const remplace = LowerCase.replace('/', '')
   const remplace2 = remplace.replace(ac, '')
-  const remplace3 = remplace2.replace(',', '')
-  const empty = remplace3.replace(/\s+/g, '')
-  return empty.trim()
+  return remplace2.trim()
 }
